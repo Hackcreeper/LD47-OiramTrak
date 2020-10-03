240df0bfe2fb85bf361e0085872ee787d283f982
Jordan Kniest <j.kniest@basecom.de>
2020-10-03 12:36:24 +0200

    Add initial car movement with controller and keyboard

---
+++ Assets/Scripts/Interaction/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using Interaction.Cars;
+using UnityEngine;
+
+namespace Interaction
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        private Car _car;
+
+        private void Start()
+        {
+            _car = DiContainer.Instance.GetByName<Car>("main_car");
+
+            Transform transform1;
+            (transform1 = transform).SetParent(_car.transform);
+            transform1.localPosition = new Vector3(0, 6.2f, -6f);
+            transform1.rotation = Quaternion.Euler(
+                38,
+                0,
+                0
+            );
+        }
+    }
+}
